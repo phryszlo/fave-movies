0c80f632102f328854269c9d8fdcf8fe28174045
phryszlo <phryszlo@gmail.com>
2022-08-24 17:40:28 -0400

    trying to set title img src from JS. including css class.

--- app.js
+++ app.js
@@ -15,10 +15,61 @@ const movies = {
       falken: "images/wargames/Stephen_Falken.webp",
       sheedy: "images/wargames/sheedy_ally1.jpg"
     }
+  },
+
+  cube: {
+
+    quotes: {
+      synopsis: "",
+      helpfulComment: "",
+
+    },
+
+    images: {
+      title: "images/cube/cube-name.jpg",
+      allyMatthew: "images/wargames/wargames-ally_matthew.jfif",
+      broderick: "images/wargames/broderick.jpg",
+      falken: "images/wargames/Stephen_Falken.webp",
+      sheedy: "images/wargames/sheedy_ally1.jpg"
+    }
   }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("resize", windowResize);
   const topImg = document.querySelector(".top-img");
   topImg.src = movies.wargames.images.title;
 })
+
+const windowResize = () => {
+  const topImg = document.querySelector(".top-img");
+
+// just wanted to know how to get vw from js
+  // const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+  // console.log(`window innerWidth: ${window.innerWidth} vs document.documentElement.clientWidth${vw}`);
+
+  let currentCss;
+  for (const sheet of document.styleSheets) {
+    currentCss = sheet;
+    // console.log(sheet)//.href.substring(sheet.href.lastIndexOf('/') + 1));
+  }
+
+  // pull the name of the active stylesheet and set class accordingly
+  // NOTE: this approach might become tedious if the class names are ever changed
+  switch (currentCss.href.lastIndexOf('/') + 1) {
+    case 'sm.css':
+      console.log('sm')
+
+      topImg.classList.remove("med", "lg");
+      topImg.classList.add("sm");
+      case 'med.css':
+      console.log('med')
+      topImg.classList.remove("sm", "lg");
+      topImg.classList.add("med");
+      case 'lg.css':
+      console.log('lg')
+      topImg.classList.remove("sm", "med");
+      topImg.classList.add("lg");
+
+  }
+}
