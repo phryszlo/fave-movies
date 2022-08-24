c779526f447c900c5c8d748f8596f9e7ded2573d
phryszlo <phryszlo@gmail.com>
2022-08-24 18:16:33 -0400

    have obtained the css media breaks by JS

--- app.js
+++ app.js
@@ -44,32 +44,80 @@ window.addEventListener("DOMContentLoaded", () => {
 const windowResize = () => {
   const topImg = document.querySelector(".top-img");
 
-// just wanted to know how to get vw from js
+  // just wanted to know how to get vw from js
   // const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
   // console.log(`window innerWidth: ${window.innerWidth} vs document.documentElement.clientWidth${vw}`);
 
   let currentCss;
+
+  // this doesn't work because it always lists all three sheets & in the same order
   for (const sheet of document.styleSheets) {
-    currentCss = sheet;
-    // console.log(sheet)//.href.substring(sheet.href.lastIndexOf('/') + 1));
+    console.log(sheet.href.substring(sheet.href.lastIndexOf('/') + 1));
+    currentCss = sheet.href.substring(sheet.href.lastIndexOf('/') + 1);
   }
+
+  /* so I'm doing this instead:
+        hold the media break and the name of the css file in a key:value (a class) array
+        so we can line them up ourselves.
+     Concern: this seems like a bunch of work to do on every resize event,
+              but I'm thinking that doesn't happen too very often? Don't know.
+  */
+  class mediaBreak {
+    constructor(href, media) {
+      this.href = href;
+      this.media = media;
+    }
+  }
+
+  // make an array to hold the css link attributes
+  const mediaBreaks = [];
+
+  const links = document.querySelectorAll('link');
+  links.forEach(link => {
+    console.log(link.getAttribute('media')
+      .substring(link.getAttribute('media').lastIndexOf(": ") + 2,
+        link.getAttribute('media').lastIndexOf(')')));
+
+    mediaBreaks.push(
+      new mediaBreak(
+        link.getAttribute('href'),
+        link.getAttribute('media')
+          .substring(link.getAttribute('media').lastIndexOf(": ") + 2,
+            link.getAttribute('media').lastIndexOf(')')
+          )
+      )
+    )
+  });
+
+  console.log(mediaBreaks)
+
+
+
+
+
+
 
   // pull the name of the active stylesheet and set class accordingly
   // NOTE: this approach might become tedious if the class names are ever changed
-  switch (currentCss.href.lastIndexOf('/') + 1) {
+  switch (currentCss) {
     case 'sm.css':
       console.log('sm')
 
       topImg.classList.remove("med", "lg");
       topImg.classList.add("sm");
-      case 'med.css':
+      break;
+    case 'med.css':
       console.log('med')
       topImg.classList.remove("sm", "lg");
       topImg.classList.add("med");
-      case 'lg.css':
+      break;
+    case 'lg.css':
       console.log('lg')
       topImg.classList.remove("sm", "med");
       topImg.classList.add("lg");
+      break;
+    default:
+      break;
 
   }
 }
